78d9776dd6c73e92d740150035f8d351560452cd
ahmed <52543589+amdMUST@users.noreply.github.com>
2019-09-19 21:41:00 -0400

    V1 It works!
    right now he can only respond to slime or slat, no both or none or nav yet

--- cartiReply.py
+++ cartiReply.py
@@ -25,6 +25,13 @@ for mention in mentions:
 # this stores the last seen id
 fileName = 'cartiReplytextfile.txt'
 
+#phrases
+slime =' jus chillin my slime praise to carti*!^'
+slat = ' slat on my momma we WANT WHOLE LOTTA RED!!^!*'
+og = ' i\'m an OG. i was an OG when i was 16. i was an OG when i made the decision I don\'t want to go to school anymore and start skipping to make music.'
+slatslime = ' ight i see u slime, keep it slat!***'
+navimg = 'nav1.jpeg'
+
 def retrieveLastSeenId(fileName):
     fRead = open(fileName,'r')
     lastSeenId = int(fRead.read().strip())
@@ -47,15 +54,23 @@ def replyToTweets():
         lastSeenId = mention.id
         storeLastSeenId(lastSeenId,fileName)
         if 'slime' in mention.full_text.lower():
-            print('tryna respond rn bro')
-            api.update_status('@' + mention.user.screen_name + 'jus chillin my slime praise to carti*!^')
+            print('slime response')
+            api.update_status('@' + mention.user.screen_name + slime, in_reply_to_status_id = lastSeenId)       
         elif 'slat' in mention.full_text.lower():
-            print('tryna respond rn bro')
-            api.update_status('@' + mention.user.screen_name + 'slat on my momma we WANT WHOLE LOTTA RED!!^!*')
-        #elif  ( ('slime' in mention.full_text.lower()) and ('slat' in mention.full_text.lower()) ):
-         #   print('tryna respond rn bro')
-         #   api.update_status('@' + mention.user.screen_name + 'ight i see u slime, keep it slat!***')
+            print('slat response')
+            api.update_status('@' + mention.user.screen_name + slat, in_reply_to_status_id = lastSeenId)
+        elif  ( ('slime' and 'slat' in mention.full_text.lower() ) ):
+            print('slime slat')
+            api.update_status('@' + mention.user.screen_name + slatslime, in_reply_to_status_id = lastSeenId)
+        elif 'nav' in mention.full_text.lower():
+            print('nav response')
+            api.update_with_media(navimg,'@' + mention.user.screen_name + slime, in_reply_to_status_id = lastSeenId)    
+        else:
+            print('og respose')
+            api.update_status('@' + mention.user.screen_name + og, in_reply_to_status_id = lastSeenId)
+
 
 while True:
     replyToTweets()
     time.sleep(15)
+    print('1tick')
