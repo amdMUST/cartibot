25b8761d5e921f3d227a83338eec4b25f43a3866
ahmed <52543589+amdMUST@users.noreply.github.com>
2019-10-16 19:01:38 -0400

    this one has the heroku logins

---
+++ python_files/config.py
@@ -0,0 +1,5 @@
+# creds to login to api
+consumerKey = 'NdFFEbjRY3P1V0mgNz3rAzTHN'
+consumerSec = 'hHwErqiswcARJeCjSQWG6DP5j1zlPkoblh3UbQTxGTAcq9PA8o'
+accessTok = '1174077014275502080-Kpg7KmUFSSyQVfuqm8jbABli5g7aEf'
+accessSec = 'nNDhR5kjtZdLLSx8UX70J45YXVkoVa7LB3IhrUe5uFHHl'
